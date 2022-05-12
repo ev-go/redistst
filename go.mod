65d2c5b029fe5fc460885001e8dfc819c44bfa6f
ev <dmitry_dtv@mail.ru>
2022-05-13 00:07:15 +0500

    220515

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	github.com/garyburd/redigo v1.6.2
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/go-redis/redis/v8 v8.6.0 // indirect
+	github.com/go-redis/redis/v8 v8.6.0
 )
--- main.go
+++ main.go
@@ -16,27 +16,38 @@ func main() {
 		DB:       0,  // use default DB
 	})
 
-	err := rdb.Set(ctx, "key", "valueeer", 0).Err()
-	if err != nil {
-		panic(err)
+	userid := "user#125"
+	currentusertoken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBVFRFTlRJT04hIjoi0J_RgNC40LLQtdGCLCDQnNCw0LrRgSA6KSIsIkRhdGEgYW5zd2VyIGlzIjoiMjExIiwiVG9rZW4gcmVxdWVzdCBhdCI6IjIwMjItMDUtMTJUMjI6MDI6MDMuNDIzNTc1NCswNTowMCIsImFkbWluIHBlcm1pc3Npb25zPyI6Im1heWJlIiwiZXhwIjoxNjUyMzc1NTIzLCJsb2dpbiI6InJvb3QifQ.9do8soXtimGxr9TDAd6EI2W0l-95U0SSJD_5GPz4kMA"
+
+	node := rdb.Set(ctx, userid, currentusertoken, 0).Err()
+	if node != nil {
+		panic(node)
 	}
 
-	err = rdb.Set(ctx, "key2", "73", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+	// err = rdb.Set(ctx, "key2", "74", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	val, err := rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
+	val, node := rdb.Get(ctx, userid).Result()
+	if node == redis.Nil {
+		fmt.Println("key1 does not exist")
+	} else if node != nil {
+		panic(node)
+	} else {
+		fmt.Println(userid, val)
 	}
-	fmt.Println("key", val)
+	// val, err := rdb.Get(ctx, "key").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("key", val)
 
-	val2, err := rdb.Get(ctx, "key2").Result()
-	if err == redis.Nil {
+	val2, node := rdb.Get(ctx, "key2").Result()
+	if node == redis.Nil {
 		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
+	} else if node != nil {
+		panic(node)
 	} else {
 		fmt.Println("key2", val2)
 	}
